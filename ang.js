fa418812c00174f31c89cbf3800ae120885b5a20
madhukesh <madhukeshpoddar@github.com>
2016-08-03 13:03:32 +0530

    my first

---
+++ ang.js
@@ -0,0 +1,89 @@
+// var name = 'amit';
+// if(name === 'amit'|| name ==='jen'){
+// 	console.log('hello world')
+// }
+// else if(name === 'chet'){
+// 	console.log('hey');
+
+// }
+// 	else {
+// // 		console.log('hey stranger!');
+// // 	}
+
+// function greetuser(name,lastname){
+// 	console.log('hello' + name +'!');
+
+// }
+// greetuser('madhukesh', 'poddar');
+// greetuser('amit' , 'j');
+
+
+// function greetuser(name,lastname){
+// 	returns 'hello' + name + '' lastname + '!';
+// }
+// 	var greeting = greetuser('amit' 'sharma');
+// 	console.log('greeting');
+// 	greetuser('mohit')
+ 
+// function add (a,b)
+// {
+// 	var result = a+b;
+// 	return result;
+	
+	// person.lastname ='poddar'
+// }
+
+// var result = add(1,88);
+// console.log(result);
+// console.log(add(3,2));
+
+// var person = {};
+// person.firstname = 'madhukesh';
+// person.lastname ='poddar';
+// function greetuser(person){
+// 	console.log('hello' +person.firstname+ ' ' +person.lastname);
+// }
+
+// greetuser(person);
+
+
+// var isvalid = false;
+// if(isvalid === true){
+// 	console.log('data is valid');
+
+// }
+// else{
+// 	console.log('data is not valid')
+// }
+
+// var isvalid = false;
+// isvalid =!isvalid;
+// if(isvalid === true){
+// 	console.log('data is valid');
+
+// }
+// else{
+// 	console.log('data is not valid')
+// }
+
+// var isvalid = false;
+
+// isvalid =!isvalid;
+// //console.log(isvalid);
+
+// //console.log('a' ==='b')
+
+
+// if(isvalid == isvalid)
+// {
+// 	console.log('data is valid');
+
+// }
+
+
+// else {
+// 	console.log('data is not valid')
+// }
+
+var isvalid = false;
+function 
---
+++ bank.js
@@ -0,0 +1,30 @@
+// var account ={
+// 	balance:0
+// };
+// function deposit (account,amount){
+// 	account.balance += amount;
+
+// }
+// //withdraw[account,amount]
+// 	function withdraw (account,amount){
+//      account.balance-= amount;
+
+// 	}
+
+// 	function getbalance(amount){
+
+// 		return account.balance;
+// 	}
+
+// 	deposit(account,1000);
+// 	// console.log(getbalance(account));
+// 	// withdraw(account,121);
+
+var isvalid = false;
+if(isvalid === true){
+	console.log('data is valid');
+
+}
+else{
+	console.log('data is not valid')
+}
